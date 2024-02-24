4193b2465a7a39ce60c774d335a0bca59abda368
Roman <stalkerxxl@gmail.com>
2024-02-24 10:55:43 +0400

    Initial

---
+++ bin/dart_start.dart
@@ -0,0 +1,24 @@
+import 'dart:convert';
+import 'dart:io';
+
+const String filename = 'bin\\with_keys.json';
+
+void main() async {
+  // Read some data.
+  final fileData = await _readFileAsync();
+  final jsonData = jsonDecode(fileData);
+
+  // Use that data.
+  print('Number of JSON keys: ${jsonData.length}');
+}
+
+Future<String> _readFileAsync() async {
+  final file = File(filename);
+  final contents = await file.readAsString();
+  return contents.trim();
+}
+
+class Human {
+  final String name;
+  Human({required this.name});
+}
