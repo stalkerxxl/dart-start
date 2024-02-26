cc5f2cb9b765c7bdf111be06ec5d9a3a284d8939
Roman <stalkerxxl@gmail.com>
2024-02-26 16:34:07 +0400

    named_constructor

---
+++ bin/named_constructor.dart
@@ -0,0 +1,52 @@
+class Color {
+  int red;
+  int green;
+  int blue;
+
+  Color(this.red, this.green, this.blue);
+
+// TODO: Create a named constructor called "Color.black" here:
+  Color.black()
+      :
+        red = 0,
+        green = 0,
+        blue = 0;
+}
+
+
+// Tests your solution (Don't edit!):
+void main() {
+  final errs = <String>[];
+
+  try {
+    final result = Color.black();
+
+    if (result.red != 0) {
+      errs.add(
+          'Called Color.black() and got a Color with red equal to \n ${result
+              .red} instead of the expected value (0).');
+    }
+
+    if (result.green != 0) {
+      errs.add(
+          'Called Color.black() and got a Color with green equal to \n ${result
+              .green} instead of the expected value (0).');
+    }
+
+    if (result.blue != 0) {
+      errs.add(
+          'Called Color.black() and got a Color with blue equal to \n ${result
+              .blue} instead of the expected value (0).');
+    }
+  } catch (e) {
+    print('Called Color.black() and got an exception of type \n ${e
+        .runtimeType}.');
+    return;
+  }
+
+  if (errs.isEmpty) {
+    print('Success!');
+  } else {
+    errs.forEach(print);
+  }
+}
