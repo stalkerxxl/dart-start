784dcbc6aa3844321ecc80223df5b1f5628898de
Roman <stalkerxxl@gmail.com>
2024-02-26 16:58:56 +0400

    const_constructor

---
+++ bin/const_constructor.dart
@@ -0,0 +1,41 @@
+class Recipe {
+  final List<String> ingredients;
+  final int calories;
+  final double milligramsOfSodium;
+
+// TODO: Create a const constructor here"
+  const Recipe(this.ingredients, this.calories, this.milligramsOfSodium);
+}
+
+// Tests your solution (Don't edit!):
+void main() {
+  final errs = <String>[];
+
+  try {
+    const obj = Recipe(['1 egg', 'Pat of butter', 'Pinch salt'], 120, 200);
+
+    if (obj.ingredients.length != 3) {
+      errs.add(
+          'Called Recipe([\'1 egg\', \'Pat of butter\', \'Pinch salt\'], 120, 200) \n and got an object with ingredient list of length ${obj.ingredients.length} rather than the expected length (3).');
+    }
+
+    if (obj.calories != 120) {
+      errs.add(
+          'Called Recipe([\'1 egg\', \'Pat of butter\', \'Pinch salt\'], 120, 200) \n and got an object with a calorie value of ${obj.calories} rather than the expected value (120).');
+    }
+
+    if (obj.milligramsOfSodium != 200) {
+      errs.add(
+          'Called Recipe([\'1 egg\', \'Pat of butter\', \'Pinch salt\'], 120, 200) \n and got an object with a milligramsOfSodium value of ${obj.milligramsOfSodium} rather than the expected value (200).');
+    }
+  } catch (e) {
+    print(
+        'Tried calling Recipe([\'1 egg\', \'Pat of butter\', \'Pinch salt\'], 120, 200) \n and received a null.');
+  }
+
+  if (errs.isEmpty) {
+    print('Success!');
+  } else {
+    errs.forEach(print);
+  }
+}
