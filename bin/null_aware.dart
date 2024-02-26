7901f4fa1bd6c68ef776298046889adf1129e92e
Roman <stalkerxxl@gmail.com>
2024-02-26 10:44:22 +0400

    null aware

---
+++ bin/null_aware.dart
@@ -0,0 +1,31 @@
+String? foo = 'a string';
+String? bar; // = null
+
+// Substitute an operator that makes 'a string' be assigned to baz.
+String? baz = foo ?? bar;
+
+void updateSomeVars() {
+  // Substitute an operator that makes 'a string' be assigned to bar.
+  bar ?? 'a string';
+}
+
+
+// Tests your solution (Don't edit!):
+void main() {
+  try {
+    updateSomeVars();
+    
+    if (foo != 'a string') {
+      print('Looks like foo somehow ended up with the wrong value.');
+    } else if (bar != 'a string') {
+      print('Looks like bar ended up with the wrong value.');
+    } else if (baz != 'a string') {
+      print('Looks like baz ended up with the wrong value.');
+    } else {
+      print('Success!');
+    }
+  } catch (e) {
+    print('Exception: ${e.runtimeType}.');
+  }
+  
+}
